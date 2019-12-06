d8b7a60b7f40d0442d08caeb7c49b7c3ba52d511
nassersaazi <nassersnoop@gmail.com>
2019-12-06 09:39:12 +0300

    Working on listbox logic

--- ConsignmentShopUI/ConsignmentShop.Designer.cs
+++ ConsignmentShopUI/ConsignmentShop.Designer.cs
@@ -28,20 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.headerText = new System.Windows.Forms.Label();
+            this.itemsListBox = new System.Windows.Forms.ListBox();
+            this.itemsListBoxLabel = new System.Windows.Forms.Label();
+            this.purchaseItemButton = new System.Windows.Forms.Button();
+            this.shoppingCartListBoxLabel = new System.Windows.Forms.Label();
+            this.shoppingCartListBox = new System.Windows.Forms.ListBox();
+            this.purchaseButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // headerText
+            // 
+            this.headerText.AutoSize = true;
+            this.headerText.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerText.Location = new System.Drawing.Point(69, 55);
+            this.headerText.Name = "headerText";
+            this.headerText.Size = new System.Drawing.Size(306, 37);
+            this.headerText.TabIndex = 0;
+            this.headerText.Text = "Consignment Shop";
+            // 
+            // itemsListBox
+            // 
+            this.itemsListBox.FormattingEnabled = true;
+            this.itemsListBox.Location = new System.Drawing.Point(79, 175);
+            this.itemsListBox.Name = "itemsListBox";
+            this.itemsListBox.Size = new System.Drawing.Size(225, 212);
+            this.itemsListBox.TabIndex = 1;
+            // 
+            // itemsListBoxLabel
+            // 
+            this.itemsListBoxLabel.AutoSize = true;
+            this.itemsListBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.itemsListBoxLabel.Location = new System.Drawing.Point(76, 126);
+            this.itemsListBoxLabel.Name = "itemsListBoxLabel";
+            this.itemsListBoxLabel.Size = new System.Drawing.Size(103, 20);
+            this.itemsListBoxLabel.TabIndex = 2;
+            this.itemsListBoxLabel.Text = "Store Items";
+            // 
+            // purchaseItemButton
+            // 
+            this.purchaseItemButton.Location = new System.Drawing.Point(355, 252);
+            this.purchaseItemButton.Name = "purchaseItemButton";
+            this.purchaseItemButton.Size = new System.Drawing.Size(87, 40);
+            this.purchaseItemButton.TabIndex = 3;
+            this.purchaseItemButton.Text = "Add To Cart ->";
+            this.purchaseItemButton.UseVisualStyleBackColor = true;
+            this.purchaseItemButton.Click += new System.EventHandler(this.purchaseItemButton_Click);
+            // 
+            // shoppingCartListBoxLabel
+            // 
+            this.shoppingCartListBoxLabel.AutoSize = true;
+            this.shoppingCartListBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.shoppingCartListBoxLabel.Location = new System.Drawing.Point(503, 126);
+            this.shoppingCartListBoxLabel.Name = "shoppingCartListBoxLabel";
+            this.shoppingCartListBoxLabel.Size = new System.Drawing.Size(124, 20);
+            this.shoppingCartListBoxLabel.TabIndex = 5;
+            this.shoppingCartListBoxLabel.Text = "Shopping Cart";
+            this.shoppingCartListBoxLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // shoppingCartListBox
+            // 
+            this.shoppingCartListBox.FormattingEnabled = true;
+            this.shoppingCartListBox.Location = new System.Drawing.Point(506, 175);
+            this.shoppingCartListBox.Name = "shoppingCartListBox";
+            this.shoppingCartListBox.Size = new System.Drawing.Size(225, 212);
+            this.shoppingCartListBox.TabIndex = 4;
+            this.shoppingCartListBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // purchaseButton
+            // 
+            this.purchaseButton.Location = new System.Drawing.Point(644, 398);
+            this.purchaseButton.Name = "purchaseButton";
+            this.purchaseButton.Size = new System.Drawing.Size(87, 40);
+            this.purchaseButton.TabIndex = 6;
+            this.purchaseButton.Text = "Purchase";
+            this.purchaseButton.UseVisualStyleBackColor = true;
+            this.purchaseButton.Click += new System.EventHandler(this.purchaseButton_Click);
             // 
             // ConsignmentShop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.purchaseButton);
+            this.Controls.Add(this.shoppingCartListBoxLabel);
+            this.Controls.Add(this.shoppingCartListBox);
+            this.Controls.Add(this.purchaseItemButton);
+            this.Controls.Add(this.itemsListBoxLabel);
+            this.Controls.Add(this.itemsListBox);
+            this.Controls.Add(this.headerText);
             this.Name = "ConsignmentShop";
             this.Text = "Consignment Shop";
+            this.Load += new System.EventHandler(this.ConsignmentShop_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label headerText;
+        private System.Windows.Forms.ListBox itemsListBox;
+        private System.Windows.Forms.Label itemsListBoxLabel;
+        private System.Windows.Forms.Button purchaseItemButton;
+        private System.Windows.Forms.Label shoppingCartListBoxLabel;
+        private System.Windows.Forms.ListBox shoppingCartListBox;
+        private System.Windows.Forms.Button purchaseButton;
     }
 }
 
--- ConsignmentShopUI/ConsignmentShop.cs
+++ ConsignmentShopUI/ConsignmentShop.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsignmentShopLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,105 @@ namespace ConsignmentShopUI
 {
     public partial class ConsignmentShop : Form
     {
-        public ConsignmentShop()
+        private Store store = new Store();
+        private List<Item> shoppingCartData = new List<Item>();
+        BindingSource itemsBinding = new BindingSource();
+        BindingSource cartBinding = new BindingSource();
+
+        public ConsignmentShop() // Constructor -- Has no return type
         {
             InitializeComponent();
+            SetupData();
+
+            itemsBinding.DataSource = store.Items;
+            itemsListBox.DataSource = itemsBinding;
+
+            itemsListBox.DisplayMember = "Display";
+            itemsListBox.ValueMember = "Display";
+
+            cartBinding.DataSource = shoppingCartData;
+            shoppingCartListBox.DataSource = cartBinding;
+
+            shoppingCartListBox.DisplayMember = "Display";
+            shoppingCartListBox.ValueMember = "Display";
+
+        }
+
+        private void SetupData()
+        {
+            Vendor demoVendor = new Vendor();
+
+            //One way of initialising an object
+            demoVendor.FirstName = "Bill";
+            demoVendor.LastName = "Gates";
+
+
+            store.Vendors.Add(demoVendor);
+
+            //Another way of initialising object
+            store.Vendors.Add(new Vendor { FirstName = "Sue", LastName = "Evans" });
+            store.Items.Add(new Item
+            {
+                Title = "Hard Drive",
+                Description = "A Book about Microsoft",
+                Price = 4.50M,
+                Owner = store.Vendors[0]
+            });
+
+            store.Items.Add(new Item
+            {
+                Title = "Shoe Dog",
+                Description = "A Book about Nike",
+                Price = 3.50M,
+                Owner = store.Vendors[1]
+            });
+
+            store.Items.Add(new Item
+            {
+                Title = "Mind Maps ",
+                Description = "A Book about the mind",
+                Price = 2.50M,
+                Owner = store.Vendors[0]
+            });
+
+            store.Items.Add(new Item
+            {
+                Title = "Homo Deus",
+                Description = "A Book about Humanity",
+                Price = 6.50M,
+                Owner = store.Vendors[1]
+            });
+
+            store.Name = "Amazonia";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ConsignmentShop_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void purchaseItemButton_Click(object sender, EventArgs e)
+        {   // Figure out what is selected from the items list
+            // Copy the item to the shopping cart
+            // Do we remove the item from the items list? - no
+            Item selectedItem = (Item)itemsListBox.SelectedItem;
+
+            MessageBox.Show(selectedItem.Title);
+        }
+
+        private void purchaseButton_Click(object sender, EventArgs e)
+        {
+             
         }
     }
 }
